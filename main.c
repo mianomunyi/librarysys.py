5a2dd21eef25717ca51a68adc7e0ec2f3984828b
mianomunyi <91963021+mianomunyi@users.noreply.github.com>
2021-11-23 15:47:24 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,47 +1,26 @@
-/*
-project: Quadratic Equation Solution
-Author:  Imran Charles
-Date:     26th oct, 2021
-Complier: GCC
-Language: C99
-license: MIT
-
-INPUTS -> a,b,c
-OUTPUTS -> x1,x2
-*/
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
 int main()
 {
-    // variable declaration
-    int a,b,c;
-    float x1,x2;
+print("\n\t\t\t");
+print("\n\t\t\t");
+printf("library system");
+printf("\n\t\t\t");
+printf("welcome Mr: Miano munyi!\n\t\t\t");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("MENU\n");
+printf("search book\n");
+printf("add book\n");
+printf("edit book\n");
+printf("delete book\n");
+printf("download book\n");
+printf("edit book\n");
+printf("exit/n")#include <stdio.h>
 
-    // capture input
-    printf("\tQuadratic Equation Solver!\n");
-    printf("Enter the values a,b and c: ");
-    scanf("%d%d%d",&a,&b,&c);
-
-    // processing/computation
-    if((b*b-4*a*b*c) == 0) {
-        x1 = x2 = (-b + sqrt(b*b-4*a*c))/(2*a);
-
-    }
-     if((b*b-4*a*b*c) > 0) {
-        x1 =  (-b + sqrt(b*b-4*a*c))/(2*a);
-        x2 =  (-b - sqrt(b*b-4*a*c))/(2*a);
-
-    }
-     if((b*b-4*a*c) < 0) {
-        printf("The Equation has no roots\n");
-        exit(1);
-     }
-
-    // output
-    printf("The values of x are %f and %f\n",x1,x2);
     return 0;
 }
